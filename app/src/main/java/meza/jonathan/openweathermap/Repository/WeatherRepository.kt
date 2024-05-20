a5b544535f0780f9388652796db5aa04621735d6
Jonathan Daniel Meza Casas <jonathan.meza4186@alumnos.udg.mx>
2024-05-20 15:07:08 -0600

    First commit

--- app/src/main/java/meza/jonathan/openweathermap/Repository/WeatherRepository.kt
+++ app/src/main/java/meza/jonathan/openweathermap/Repository/WeatherRepository.kt
@@ -6,4 +6,7 @@ class WeatherRepository(val api:ApiServices) {
 
     fun getCurrentWeather(lat:Double, lng:Double, unit:String) =
         api.getCurrentWeather(lat, lng, unit, "0317cb7600173872a3236b3a37f3d6ba")
+
+    fun getForecastWeather(lat:Double, lng:Double, unit:String) =
+        api.getForecastWeather(lat, lng, unit, "0317cb7600173872a3236b3a37f3d6ba")
 }
--- app/src/main/java/meza/jonathan/openweathermap/Server/ApiServices.kt
+++ app/src/main/java/meza/jonathan/openweathermap/Server/ApiServices.kt
@@ -1,6 +1,7 @@
 package meza.jonathan.openweathermap.Server
 
 import meza.jonathan.openweathermap.model.CurrentResponseApi
+import meza.jonathan.openweathermap.model.ForecartResponseApi
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Query
@@ -14,4 +15,12 @@ interface ApiServices {
         @Query("units") units:String,
         @Query("appid") ApiKey:String,
     ): Call<CurrentResponseApi>
+
+    @GET("data/2.5/forecast")
+    fun getForecastWeather(
+        @Query("lat") lat:Double,
+        @Query("lon") lon:Double,
+        @Query("units") units:String,
+        @Query("appid") ApiKey:String,
+    ): Call<ForecartResponseApi>
 }
